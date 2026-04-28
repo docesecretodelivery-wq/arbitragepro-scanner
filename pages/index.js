886ff50547b742c11198bf3e50d0b5eb47913216
docesecretodelivery-wq <docesecreto.delivery@gmail.com>
2026-04-28 18:02:11 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{fontFamily:"Arial",padding:"40px"}}>
+      <h1>ArbitragePro Scanner</h1>
+      <p>Scanner ativo com sucesso.</p>
+    </div>
+  );
+}
